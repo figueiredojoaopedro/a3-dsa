31b9a488ee5cfdde07deb1f78287ba52619f93c5
figueiredojoaopedro <neewprojecti@gmail.com>
2024-05-08 19:40:21 -0300

    tentativa guiches

---
+++ Guiche.py
@@ -0,0 +1,10 @@
+
+class Guiche:
+    def __init__(self):
+        self.ocupacao = None;
+
+    def getOcupacao(self):
+        return self.ocupacao;
+
+    def setOcupacao(self, ocupacao):
+        self.ocupacao = ocupacao;
--- main.py
+++ main.py
@@ -1,11 +1,40 @@
-import random;
-from Queue import Queue;
+import time;
 from functions.transacaoEscolhida import transacaoEscolhida;
 from functions.chegouCliente import chegouCliente;
+from functions.initializingGuiches import initializingGuiches
 
 def main():
-    transacaoCliente = transacaoEscolhida();
-    print("teste {}".format(transacaoCliente));
+    expedientTotalTime = 6; # 6 hours
+    realTimeElapsed = 120; # 2 minutes
+    simulationTime = 0;
+    beginningTime = 10; # opens at 10 am
+    equivalentToOneHour = int(realTimeElapsed / expedientTotalTime);
+
+    guiche = initializingGuiches();
+
+    print("{} Horas".format(beginningTime))
+    while simulationTime < realTimeElapsed:
+        # tell us the simulation time
+        if(simulationTime % equivalentToOneHour == 0):
+            beginningTime += 1; # add + 1 hour
+            print("{} Horas".format(beginningTime));
+        
+        # cliente chega:
+        if(chegouCliente() == 0):
+            print("Cliente chegou");
+            # cliente entra na fila
+
+
+        # checa se há guiche vazio
+
+        # primeiro da fila entra no guiche
+
+        # cliente escolhe transacao
+
+        # time.sleep(1);
+        simulationTime += 1;
+
+    print("teste {}".format(transacaoEscolhida()));
 
     print("teste 2 {}".format(chegouCliente()));
 
